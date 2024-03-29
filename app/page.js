e044ec19a62fb5499cd85caff82b9674820ab1ea
YVNSivaReddy <yvnsivaprasad97@gmail.com>
2024-03-29 20:30:50 +0530

    updated NEXT JS

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-10">
-       <p>hii</p>
+       <p>hii its updated</p>
     </main>
   );
 }
